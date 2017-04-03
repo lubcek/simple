f0dc44a37df2bbf3fecda72c92d75a8077b0f5d9
Lubor Čech <Lubor ech>
2017-04-03 20:00:26 +0200

    upload a new script

---
+++ seznam.py
@@ -0,0 +1,15 @@
+l = []
+a = ''
+print('Pro konec zadávání napište k')
+while a!='k':
+    a = input('Zadej položku na nákup do seznamu: ')
+    if a in l:
+        print('Tuto položku jsi již zadal.')
+    else:
+        l.append(a)
+l.pop(-1)
+print('')
+print('Musíš nakoupit:')
+print('---------------')
+for item in l:
+    print(item)
